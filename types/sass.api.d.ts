33daada57990416f5b8c4e0b6e61fa71fcd77802
Corbin Crutchley <crutchcorn@gmail.com>
2021-05-11 22:59:24 -0700

    Type most API methods

--- types/sass.api.d.ts
+++ types/sass.api.d.ts
@@ -8,22 +8,34 @@ export enum SassStyle {
 export interface SassAPI {
   style: SassStyle;
 
-  options(options: any, callback: any): any;
+  // TODO: Match the docs
+  // https://github.com/medialize/sass.js/blob/master/docs/api.md#libsass-compile-options
+  options(options: 'defaults' | unknown, callback?: () => void): void;
 
-  writeFile(filename: any, text: any, callback: any): any;
+  writeFile(filename: string, text: string, callback?: (val: boolean) => void): void;
+  writeFile(filename: Record<string, string>, callback?: (map: Record<string, boolean>) => void): void;
 
-  readFile(filename: any, callback: any): any;
+  readFile(filename: string, callback?: (content: string | undefined) => void): void;
+  readFile(filename: string[], callback?: (map: Record<string, string | undefined>) => void): void;
 
-  listFiles(filename: any, callback: any): any;
+  listFiles(callback?: (list: string[]) => void): void;
 
-  removeFile(filename: any, callback: any): any;
+  removeFile(filename: string, callback?: (success: boolean) => void): void;
+  removeFile(filename: string[], callback: (results: Record<string, boolean>) => void): void;
 
-  clearFiles(callback: any): any;
+  clearFiles(callback: () => void): void;
 
-  lazyFiles(base: any, directory: any, files: any, callback: any): any;
+  /**
+   * @deprecated
+   * @see https://github.com/medialize/sass.js/blob/master/docs/api.md#lazyloading-files-deprecated
+   */
+  lazyFiles(base: string, directory: string, files: string[], callback: () => void): void;
 
-  preloadFiles(base: any, directory: any, files: any, callback: any): any;
+  preloadFiles(base: string, directory: string, files: string[], callback: () => void): void;
 
+  // TODO: Type
+  // @see https://github.com/medialize/sass.js/blob/master/docs/api.md#compiling-strings
+  // @see https://github.com/medialize/sass.js/blob/master/docs/api.md#the-response-object
   compile(text: any, _options: any, callback: any, _compileFile: any): any;
 
   compileFile(filename: any, _options: any, callback: any): any;
